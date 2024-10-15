2b4adf4aa93f0d6bf4dbae9a03bd029858d3ea3f
XAITYH <petrovskijpavel1@gmail.com>
2024-10-15 05:12:09 +0300

    forgot about it

--- gender_stereotypes/vite.config.js
+++ gender_stereotypes/vite.config.js
@@ -4,5 +4,5 @@ import preact from '@preact/preset-vite';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact()],
-	base: "/vite-react-deploy/",
+	base: "/Genders_Stereotypes/",
 });
